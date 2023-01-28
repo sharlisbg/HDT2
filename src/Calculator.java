e2b68ca7849a86e2e5ea362bd463a62498a2441e
sharlisbg <sharislizza15@gmail.com>
2023-01-28 17:25:07 -0600

    Comentarios

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,12 +13,21 @@ public class Calculator implements IPostfixCalculator {
         this.linea = "";
     }
 
-
-
     @Override
     public int Calculate(ArrayList postfix_expression) throws Exception {
+        /*Que lea la línea que es un string y lo empice a dividir y lo coloque en el strack */
+
+
+        /* Si es un valor númerico hace esto */
+
+        /*Si es un valor no número un operando hace esto otro */
         
+
+
+
         return 0;
     }
+
+
 
 }
