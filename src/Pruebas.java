442c5150d1d8fca71d06d11cc695a97d74df1413
sharlisbg <sharislizza15@gmail.com>
2023-01-30 23:29:26 -0600

    Finalización de pruebas

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -11,15 +11,44 @@ import org.junit.Test;
 
 public class Pruebas {
 
-    Calculator calc = new Calculator();
+    PostfixCalculator calc = new PostfixCalculator();
     ArrayList<String> list = new ArrayList<String>();
 
 
     @Test
-	public void calculando() throws Exception {
+	public void calculandoConExito() throws Exception {
         list.add("1 2 + 4 * 3 +");
 		int x = calc.Calculate(list);
 		assertEquals(15, x);
+	}
+
+    @Test
+	public void calculandoConExito2() throws Exception {
+        list.add("1 2 + 3 *");
+		int x = calc.Calculate(list);
+		assertEquals(9, x);
+	}
+
+    @Test
+	public void calculandoParaLetras() throws Exception {
+        list.add("Bichita");
+		int x = calc.Calculate(list);
+		assertEquals(Exception, x);
+	}
+
+    
+    @Test
+	public void calculandoDivisionBajoCero() throws Exception {
+        list.add("4 0 /");
+		int x = calc.Calculate(list);
+		assertEquals(Exception, x);
+	}
+
+    @Test
+	public void calculandoConNumerosFaltante() throws Exception {
+        list.add("3 *");
+		int x = calc.Calculate(list);
+		assertEquals(Exception, x);
 	}
 
 }
