7a504b6068f2b654ed362b85b88a8a52a7d049c3
sharlisbg <sharislizza15@gmail.com>
2023-01-30 23:20:01 -0600

    Robustecimiento de Clase calculadora y Principal

--- src/StackUsingArrayList.java
+++ src/StackUsingArrayList.java
@@ -12,27 +12,25 @@ public class StackUsingArrayList<T> implements IStack<T> {
 		miListaInterna = new ArrayList<T>(); 
 	}
 	
-	@Override
+	
 	public int count() {
 		return miListaInterna.size();
 	}
 
-	@Override
+	
 	public boolean isEmpty() {
 		return miListaInterna.isEmpty();
 	}
 
-	@Override
+
 	public void push(T value) {
 		miListaInterna.add(0, value);
 	}
 
-	@Override
 	public T pull() {
 		return miListaInterna.remove(0);
 	}
 
-	@Override
 	public T peek() {		
 		return miListaInterna.get(0);
 	}
